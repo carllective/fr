26f70b4d14658a49de0b1f75113988d9bd086be8
Rebecca <109993861+carllective@users.noreply.github.com>
2022-09-05 20:18:47 -0400

    uncomment the trys toredirect

--- src/components/MeetsPage.vue
+++ src/components/MeetsPage.vue
@@ -200,7 +200,7 @@ export default {
     // document.webkitCancelFullScreen();
     window.scrollTo(0, 0);
     new Promise((res) => {
-      // try {
+      try {
         this.info = this.$meets.filter(i => `/${i.url}` === window.location.pathname)[0];
         console.log(this.info.Instagram);
         res(this.info);
@@ -208,9 +208,9 @@ export default {
           // this.instagrams = this.info.Instagram.split("\n");
           this.instagrams = this.info.Instagram;
         }
-      // } catch {
-      //   window.location.href = "/";
-      // }
+      } catch {
+        window.location.href = "/";
+      }
     }).then(() =>{
       this.renderMap();
       this.$nextTick(() => {
